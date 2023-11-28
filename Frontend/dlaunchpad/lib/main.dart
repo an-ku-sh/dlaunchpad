a0ac6347461c2fae6d0e66e149cdc5ef60d44d41
an-ku-sh <110815021+an-ku-sh@users.noreply.github.com>
2023-11-28 07:55:51 +0530

    blockscout rpc api to fetch sepolia balance in wei

--- Frontend/dlaunchpad/lib/main.dart
+++ Frontend/dlaunchpad/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -12,12 +14,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  //harcoded public address
+  String address = '0xaF8c55A5b4421252CA8fB6f77063c15805d7e40D';
+  String _sepoliaBalance = '0';
+
+  //fetch balance function
+  Future<String> fetchBalance(String address) async {
+    final response = await http.get(Uri.parse(
+        'https://eth-sepolia.blockscout.com/api?module=account&action=balance&address=$address'));
+
+    if (response.statusCode == 200) {
+      // 200 OK response => then parse the JSON.
+      Map<String, dynamic> json = jsonDecode(response.body);
+      print('Balance: ${json['result']}');
+      setState(() {
+        _sepoliaBalance = 'Balance: ${json['result']}';
+      });
+      return 'Balance: ${json['result']}';
+    } else {
+      // else => exception
+      throw Exception('Failed to load balance');
+    }
+  }
+
+  @override
+  void initState() {
+    fetchBalance(address);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("Dlaunchpad"),
+          child: Text(_sepoliaBalance),
         ),
       ),
     );
